12b67e1aa35106f1689c452cac9da45951715fab
Steve Kaser <skaser85@gmail.com>
2021-06-19 12:50:48 -0400

    created the CalcAgeInSeconds project

---
+++ CalcAgeInSeconds/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CalcAgeInSeconds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your birthdate (e.g., 11/13/1985 (US date format)...");
+            DateTime birthDate = DateTime.Parse(Console.ReadLine());
+            TimeSpan interval = DateTime.Now - birthDate;
+            int days = interval.Days;
+            int hours = interval.Hours;
+            int mins = interval.Minutes;
+            int secs = interval.Seconds;
+            int total = (secs) + (mins * 60) + (hours * 60 * 60) + (days * 24 * 60 * 60);
+            Console.WriteLine($"It has been {total.ToString("#,#")} seconds since {birthDate.ToShortDateString()}");
+        }
+    }
+}
